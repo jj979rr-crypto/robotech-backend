4ee2d0ccdb618328a2026b6d2cf946c3455774fa
Jimmy <jj979rr@gmail.com>
2026-02-16 01:30:01 -0500

    fix: habilitar conexion segura ssl para tidb

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,6 +42,10 @@ import { BehaviorModule } from './behavior/behavior.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
 
+        ssl: {
+          rejectUnauthorized: true,
+        },
+
         // Recomendado en Nest + TypeORM
         autoLoadEntities: true,
         synchronize: configService.get<string>('DB_SYNC') === 'true',
